f15abae991a65384d0250b7f166e9e24f099d96d
sintaro-katuta <sintaro.katuta@gmail.com>
2023-11-29 00:20:58 +0900

    feat: プロフィール画面のデザイン完成

--- portfolio/src/components/menu.jsx
+++ portfolio/src/components/menu.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Menu(){
     const menu = ['Profile', 'Works', 'Blog']
     return(
-        <div className='flex justify-between'>
+        <div className='flex justify-between pt-3'>
             {menu.map((item) => (
                 <Link to={`/${item.toLowerCase()}`} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>{item}</Link>
                 // <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => move()} >{item}</button>                    
--- portfolio/src/components/profile.jsx
+++ portfolio/src/components/profile.jsx
@@ -1,13 +1,30 @@
 import Header from "./header";
 import Menu from "./menu";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
     return(
         <div className="w-screen h-screen fixed">
             <Header/>
             <div className="w-full h-full px-7">
-                <div className="w-full h-3/4 flex items-center justify-center">
-                    <p className="text-xl text-center main-font">Profile</p>
+                <div className="w-full h-3/4">
+                    <div className="h-1/2 flex items-center justify-center">
+                        <img src="/profile.png" alt="" className="h-full p-3" />
+                    </div>
+                    <div className="h-1/2 border border-black rounded-lg p-3 flex flex-col items-start justify-start gap-5 main-font">
+                        <p className="text-lg">名前: <ruby>勝田<rt>かつた</rt></ruby>　<ruby>紳太郎<rt>しんたろう</rt></ruby></p>
+                        <p className="text-lg">趣味: カプセルトイ</p>
+                        <p className="text-lg">好きなプログラミング言語: JavaScript/TypeScript</p>
+                        <p className="text-lg">現在の目標:<br /> インフラができるようになりたい！</p>
+                        <div className="flex items-center justify-center gap-5">
+                            <Link target="_blank" to="https://github.com/sintaro-katuta">
+                                <img src="/github-mark.png" alt="" className="w-10 h-10" />
+                            </Link>                            
+                            <Link target="_blank" to="https://qiita.com/sintaro-katuta">
+                                <img src="/qiita.png" alt="" className="w-10 h-10" />
+                            </Link>    
+                        </div>
+                    </div>
                 </div>
                 <Menu />
             </div>
