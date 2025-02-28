0988c5f68aa16fbef6347fc57258bc28253d153d
sintaro-katuta <sintaro.katuta@gmail.com>
2025-03-01 02:47:25 +0900

    update #1 nuxt

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:latest
-
-WORKDIR /usr/src/app
-
-RUN npm install -g npm@latest
-
-RUN npm install -g create-react-app
-
-EXPOSE 3002
--- portfolio/src/components/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function Index() {
-    return(
-        <div className='w-full h-body px-7'>
-            <section className="main w-full h-3/4 flex items-center justify-center">
-                <span>Hello!</span>
-            </section>
-        </div>
-    )
-}
